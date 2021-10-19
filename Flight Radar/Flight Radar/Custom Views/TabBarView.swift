4f965bd7b8cf8aab5631ab15fd6f96e675c7c1b0
Ivan Budovich <ibudovichivan3@gmail.com>
2021-10-19 13:11:39 +0300

    Setup TabBarView

--- Flight Radar/Flight Radar/Custom Views/TabBarView.swift
+++ Flight Radar/Flight Radar/Custom Views/TabBarView.swift
@@ -7,31 +7,75 @@
 
 import SwiftUI
 
-//struct TabBarView: View {
-//    let tabs =
-//    [Tab(image: Image(systemName: "airplane"), label: "Airports"),
-//     Tab(image: Image(systemName: "map"), label: ")
-//    ]
-//    @Binding var selection: Int
-//    @Namespace private var currentTab
-//    var body: some View {
-//        ZStack {
-//            Color.manhattan
-//            HStack {
-//                
-//            }
-//        }
-//        
-//    }
-//}
-//
-//struct TabBarView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        TabBarView(selection: .constant(0))
-//    }
-//}
-//
-//struct Tab {
-//    let image: Image
-//    let label: String
-//}
+struct TabBarView: View {
+    @Binding var selectedTab: Int
+    var body: some View {
+        HStack(spacing: 0) {
+            TabBarButton(image: Image("map"), index: 0, selectedTab: $selectedTab)
+            TabBarButton(image: Image("compass"), index: 1, selectedTab: $selectedTab)
+            TabBarButton(image: Image("plane"), index: 2, selectedTab: $selectedTab)
+            TabBarButton(image: Image("airport"), index: 3, selectedTab: $selectedTab)
+        }
+        .padding(10)
+        .background(Color.whiteLiliac)
+        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+        .padding(.horizontal)
+        .shadow(color: .charcoal.opacity(0.2), radius: 4, x: 5, y: 5)
+        .shadow(color: .white.opacity(0.8), radius: 4, x: -5, y: -5)
+        
+    }
+}
+
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView(selectedTab: .constant(0))
+    }
+}
+
+struct TabBarButton: View {
+    var image: Image
+    let index: Int
+    @Binding var selectedTab: Int
+    
+    var body: some View {
+        GeometryReader {
+            reader in
+            ZStack {
+                Color.whiteLiliac
+                    
+                    .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                    .frame(width: 35, height: 35, alignment: .center)
+                    .shadow(color: .charcoal.opacity(0.4), radius: 4, x: 5, y: 5)
+                    .shadow(color: .white.opacity(0.8), radius: 4, x: -5, y: -5)
+                    .isHidden(selectedTab == index)
+                
+                Color.whiteLiliac
+                    .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                    .frame(width: 40, height: 40, alignment: .center)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10, style: .continuous)
+                            .stroke(Color.whiteLiliac, lineWidth: 4)
+                            .shadow(color: Color.charcoal.opacity(0.4), radius: 3, x: 2, y: 2)
+                            .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                            .shadow(color: .white, radius: 2, x: -1, y: -1)
+                            .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                    )
+                    
+                    .isHidden(selectedTab != index)
+                
+                Button(action: { selectedTab = index }) {
+                    image
+                        .renderingMode(.original)
+                        .resizable()
+                        .frame(width: 25, height:25, alignment: .center)
+
+                    
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+            .frame(height: 30, alignment: .center)
+        }
+        .frame(height: 30)
+    }
+}
