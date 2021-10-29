93165b27d4114f7d9c852da962170634ab354b59
Ivan Budovich <ibudovichivan3@gmail.com>
2021-10-29 11:36:40 +0300

    Add faux APIKey

---
+++ FlightRadar/FlightRadar/Services/Networking/APIKey.swift
@@ -0,0 +1,12 @@
+//
+//  APIKey.swift
+//  FlightRadar
+//
+//  Created by Ivan Budovich on 10/29/21.
+//
+
+import Foundation
+enum APIKey {
+    static let aviationStackKey = ""
+    static let rapidApiKey = ""
+}
